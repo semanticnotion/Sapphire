42a23f03045765ce7c7e82ddead0987d0025dfdd
aikhan <asadkhan.khan@gmail.com>
2012-01-04 21:15:52 +0500

    Initial Commit

---
+++ Sapphire Eclipse/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Sapphire Eclipse
+//
+//  Created by Asad Khan on 1/4/12.
+//  Copyright (c) 2012 Semantic Notion Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Sapphire Eclipse/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sapphire Eclipse
+//
+//  Created by Asad Khan on 1/4/12.
+//  Copyright (c) 2012 Semantic Notion Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
